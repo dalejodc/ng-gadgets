77254fd1fefaa3efc52e1b304bd9bb0abddf19be
Daniel <dalejodc@gmail.com>
2018-10-01 21:04:28 -0600

    Routes, home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes, RouterModule} from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
+import { GadgetComponent } from "./components/gadget/gadget.component";
+import { GadgetsComponent } from "./components/gadget/gadgets.component";
 
 
 const APP_ROUTES: Routes = [
 	{path: 'home', component: HomeComponent}, 
+	{path: 'gadgets', component: GadgetsComponent}, 
+	{path: 'gadget/:id', component: GadgetComponent}, 
 
 	{ path: '**', pathMatch: 'full', redirectTo: 'home'} 
 ]
