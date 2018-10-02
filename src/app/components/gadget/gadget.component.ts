2c1b1b7e022419004ea01a3a1a7759c87ccbcf22
Daniel <dalejodc@gmail.com>
2018-10-02 17:08:16 -0600

    Gadteg form two way data bindign, file gadget service created

--- src/app/components/gadget/gadget.component.ts
+++ src/app/components/gadget/gadget.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms"
 import { Gadget } from "../../interfaces/gadget.interface";
 
+import { GadgetService } from "../../services/gadget.service";
+
 @Component({
   selector: 'app-gadget',
-  templateUrl: './gadget.component.html'
+  templateUrl: './gadget.component.html',
+  providers: [GadgetService]
 })
 export class GadgetComponent implements OnInit {
 
@@ -18,6 +21,10 @@ export class GadgetComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  save(){
+  	console.log(this.gadget);
   }
 
 }
