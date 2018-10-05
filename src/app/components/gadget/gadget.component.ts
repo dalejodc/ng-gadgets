bdbf63f12a2bfc25fa64acffadab6a7ddf448aad
Daniel <dalejodc@gmail.com>
2018-10-04 22:26:39 -0600

    Router navigate

--- src/app/components/gadget/gadget.component.ts
+++ src/app/components/gadget/gadget.component.ts
@@ -1,39 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms"
+import { Router } from "@angular/router"
 import { Gadget } from "../../interfaces/gadget.interface";
 
 
 import { GadgetService } from "../../services/gadget.service";
 
 @Component({
-  selector: 'app-gadget',
-  templateUrl: './gadget.component.html',
-  providers: [GadgetService]
+	selector: 'app-gadget',
+	templateUrl: './gadget.component.html',
+	providers: [GadgetService]
 })
 export class GadgetComponent implements OnInit {
 
-  gadget:Gadget = {
-  	name: "",
-    description: "",
-    brand: "",
-    price: null
-  }
+	gadget:Gadget = {
+		name: "",
+		description: "",
+		brand: "",
+		price: null
+	}
 
-  constructor(private _gadgetService: GadgetService) { }
+	constructor(
+		private _gadgetService: GadgetService,
+		private _router: Router
+		) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
-  save(){
-  	console.log(this.gadget);
+	save(){
+		console.log(this.gadget);
 
-    this._gadgetService.addGadget(this.gadget)
-      .subscribe(data=>{
-
-      },
-      error =>{
-        console.error(error);
-      });
-  }
+		this._gadgetService.addGadget(this.gadget)
+			.subscribe(data=>{
+				this._router.navigate(['/home']);
+			},
+			error =>{
+				console.error(error);
+			});
+	}
 
 }
